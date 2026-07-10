3a66ed881a7ddf9c159d332e365674333ecb10bf
Aayush Kumar Singh <aayushsingh5353@gmail.com>
2026-07-10 11:07:24 +0530

    Array basic with 6 questions

---
+++ 6DSA.cpp
@@ -0,0 +1,14 @@
+//       #DSA QUESTIONS 
+
+// @no1        Find element at a given Index
+
+class Solution {
+  public:
+    int findElementAtIndex(int i, vector<int> &arr) {
+        return arr[i];
+        
+    }
+};
+
+
+
